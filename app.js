355bfbaeb44412a3f69a7d77e4bd56cfa56daf82
himanshukumawat7706 <your-email@example.com>
2025-04-05 18:02:19 +0530

    commit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow common HTTP methods
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow common HTTP methods
   })
 );
 
