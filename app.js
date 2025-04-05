75ed642916e7f917aa68321d4d2e20129bb40272
himanshukumawat7706 <your-email@example.com>
2025-04-05 18:05:06 +0530

    commit

--- app.js
+++ app.js
@@ -23,19 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // CORS configuration
-const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:3000"];
+// const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:3000"];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allow common HTTP methods
   })
 );
 
