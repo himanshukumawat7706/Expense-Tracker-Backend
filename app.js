5908cd6bf01dcbdb2cc94a391af08696279e8b33
himanshukumawat7706 <your-email@example.com>
2025-04-15 10:09:42 +0530

    this is updated version

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(
   cors({
-    origin: "  http://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
